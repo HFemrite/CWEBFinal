ce3bf5db8cf40d44c0f8a25ea4fb3eb82c0e94d2
HFemrite <46502115+HFemrite@users.noreply.github.com>
2020-05-15 17:40:44 -0500

    Add files via upload

---
+++ dbo.Books.data.sql
@@ -0,0 +1,11 @@
+SET IDENTITY_INSERT [dbo].[Books] ON
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (1, N'The Lightning Thief', N'Hardcover', N'August 2017', 1, N'Young Adult', N'Rick Riordan')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (5, N'The Sea of Monsters', N'Hardcover', N'September 2017', 1, N'Young Adult', N'Rick Riordan')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (6, N'The Titan''s Curse', N'Hardcover', N'September 2017', 1, N'Young Adult', N'Rick Riordan')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (7, N'The Battle of the Labyrinth', N'Hardcover', N'October 2017', 1, N'Young Adult', N'Rick Riordan')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (8, N'The Last Olympian', N'Hardcover', N'November 2017', 1, N'Young Adult', N'Rick Riordan')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (9, N'Legend', N'Hardcover', N'March 2020', 2, N'Young Adult', N'Marie Lu')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (10, N'Prodigy', N'Hardcover', N'April 2020', 2, N'Young Adult', N'Marie Lu')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (11, N'Champion', N'Hardcover', N'May 2020', 2, N'Young Adult', N'Marie Lu')
+INSERT INTO [dbo].[Books] ([BookId], [BookTitle], [CoverType], [LastRead], [TimesRead], [Genre], [Author]) VALUES (13, N'The Young Elites', N'Softcover', N'August 2019', 1, N'Young Adult', N'Marie Lu')
+SET IDENTITY_INSERT [dbo].[Books] OFF
